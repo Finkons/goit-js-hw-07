2b0e19684c0d31119580908bbd4a18331d868041
Andrii Mykhailenko <finkons87@gmail.com>
2022-04-24 13:07:28 +0300

    add npm package with galery

---
+++ js/01-gallery.js
@@ -0,0 +1,67 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+//get galleryItems items for work
+const makeGalleryItemsLibrary = element => {
+  const { preview, original, description } = element;
+
+  return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+};
+
+//set galleryItems on DOM
+const images = document.querySelector('.gallery');
+
+const makeSiteStructure = galleryItems.map(makeGalleryItemsLibrary).join('');
+
+images.innerHTML = makeSiteStructure;
+
+//Opening pictures
+
+const instance = basicLightbox.create(
+  `
+		<img class="modal-img" src= ''>
+	`,
+  {
+    onShow: instance => {
+      window.addEventListener('keydown', onEscPress);
+    },
+    onClose: instance => {
+      window.addEventListener('keydown', onEscPress);
+    },
+  },
+);
+
+images.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  instance.element().querySelector('.modal-img').src = e.target.dataset.source;
+  //console.dir(e.target.dataset.source);
+  instance.show();
+}
+
+// Closing pictures by pressing on Esc
+function onEscPress(elem) {
+  if (elem.key === 'Escape') {
+    instance.close();
+    window.removeEventListener('keydown', onEscPress);
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const makeGalleryItemsLibrary = element => {
+  const { preview, original, description } = element;
+
+  return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /> </a>`;
+};
+
+//set galleryItems on DOM
+const images = document.querySelector('.gallery');
+
+const makeSiteStructure = galleryItems.map(makeGalleryItemsLibrary).join('');
+
+images.innerHTML = makeSiteStructure;
+
+//Opening pictures
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  close: true,
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: '250',
+});
